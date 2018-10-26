d78d7c810e7e6523583a37f111df8581653056e2
James Carlson <jxxcarlson@mac.com>
2018-10-26 00:31:46 -0400

    Update for multi-argument macros

--- src/Main.elm
+++ src/Main.elm
@@ -502,17 +502,23 @@ macro is defined as
 
 We have added an experimental macro expansion feature that permits
 the author to define new text-mode macros in a MiniLatex document.
-We add, for example, the text
+For example, if we add the text
 
 \\begin{verbatim}
 \\newcommand{\\hello}[1]{Hello \\strong{#1}!}
-
-\\hello{John}
 \\end{verbatim}
 
-Then the macro \\backslash{hello}\\texarg{John} renders as
+then the macro \\backslash{hello}\\texarg{John} renders as \\hello{John}
 
-\\hello{John}
+Likewiese, if we make the definition,
+
+\\begin{verbatim}
+\\newcommand{\\reverseconcat}[3]{#3#2#1} 
+\\end{verbatim}
+
+\\newcommand{\\reverseconcat}[3]{#3#2#1} 
+
+then \\backslash{reverseconcat}\\texarg{A}\\texarg{B}\\texarg{C} = \\reverseconcat{A}{B}{C}
 
 The macro expansion feature will need a lot more work and testing.
 We also plan to add a feature so that authors can define new environments.
