d1e905ee3dccbbc243f056b2e30e259302bb170b
James Carlson <jxxcarlson@mac.com>
2020-01-04 05:48:46 +0100

    Fix help system

--- src/StringsV2.elm
+++ src/StringsV2.elm
@@ -70,21 +70,19 @@ be visible in the
 rendered text.
 \\end{comment}
 
-$$\\boa = \\caA^4$$
-
 \\tableofcontents
 
-\\strong{Note.} This version of the MiniLaTeX demo uses MathJax 3,
-which is much faster than the 2.7.* versions and which gives
-a much better experience when doing live editing.  There is one
-temporary downside \\mdash some more work needs to be done
-to enable math-mode macros.  Coming soon!
+\\strong{Note.} This version of the MiniLaTeX
+demo uses MathJax 3, which is much faster than
+the 2.7.* versions and which gives a much better
+experience when doing live editing.
 
 \\section{Introduction}
 
-MiniLatex is a subset of LaTeX that can be
-rendered live in the browser using a custom parser.
-Mathematical text is rendered by \\href{https://mathjax.org}{MathJax}:
+MiniLatex is a subset of LaTeX that can
+be endered live in the browser using a custom parser.
+Mathematical text is rendered by
+ \\href{https://mathjax.org}{MathJax}:
 
 $$
 \\int_{-\\infty}^\\infty e^{-x^2} dx = \\pi
@@ -106,7 +104,8 @@ or write to jxxcarlson at gmail.
 
 
 
-\\italic{Try editing formula \\eqref{integral:xn} or \\eqref{integral:exp} below.}
+\\italic{Try editing formula
+\\eqref{integral:xn} or \\eqref{integral:exp} below.}
 
 The most basic integral:
 
@@ -127,9 +126,9 @@ An improper integral:
 
 \\newcommand{\\hello}[1]{Hello \\strong{#1}!}
 
-As illustrated by the examples below, one can define new text-mode macros
-in MiniLaTeX.
-For example, if we add the text
+As illustrated by the examples below,
+one can define new text-mode macros
+in MiniLaTeX. For example, if we add the text
 
 \\newcommand{\\boss}{Phineas Fogg}
 
@@ -150,7 +149,9 @@ Likewise, if one says
 \\newcommand{\\hello}[1]{Hello \\strong{#1}!}
 \\end{verbatim}
 
-then the macro \\backslash{hello}\\texarg{John} renders as \\hello{John}
+then the macro
+\\backslash{hello}\\texarg{John}
+renders as \\hello{John}
 For one more example,  make the definition
 
 \\begin{verbatim}
@@ -161,15 +162,17 @@ For one more example,  make the definition
 
 Then \\backslash{reverseconcat}\\texarg{A}\\texarg{B}\\texarg{C} = \\reverseconcat{A}{B}{C}
 
-The macro expansion feature will need a lot more work and testing.
-We also plan to add a feature so that authors can define new environments.
+The macro expansion feature will need a lot
+more work and testing.  We also plan to add a
+feature so that authors can define new environments.
 
 
 
 \\section{MiniLatex Macros}
 
-MiniLatex has a number of macros of its own,  For
-example, text can be rendered in various colors, \\red{such as red}
+MiniLatex has a number of macros of
+its own,  For example, text can be rendered
+in various colors, \\red{such as red}
 and \\blue{blue}. Text can \\highlight{be highlighted}
 and can \\strike{also be struck}. Here are the macros:
 
@@ -235,33 +238,42 @@ Beryllium& Be& 4& 9.012 \\\\
 
 Pythagoras is said to have said that $z^2 + b^2 = c^2
 
-Errors, as illustrated above, are rendered in real time and are reported in red, in place. Compare
+Errors, as illustrated above, are rendered in real
+time and are reported in red, in place. Compare
 with the source text on the left.
 
-We plan to make error reporting still better, giving detailed context.  Note, by the way, what happens when a nonexistent macro like \\italic{hohoho } is used:
+We plan to make error reporting still better, giving
+detailed context.  Note, by the way, what happens
+when a nonexistent macro like \\italic{hohoho } is used:
 
 \\begin{indent}
 \\hohoho{123}
 \\end{indent}
 
-This is intentional.  Note also what happens when we use a nonexistent environment like \\italic{joke}:
+This is intentional.  Note also what happens when
+we use a nonexistent environment like \\italic{joke}:
 
 \\begin{joke}
-Did you hear the one about the mathematician, the philosopher, and the engineer?
+Did you hear the one about the mathematician,
+the philosopher, and the engineer?
 \\end{joke}
 
-This default treatment of unkown environments is also intentional, and can even be useful.
+This default treatment of unkown environments
+ is also intentional, and can even be useful.
 
 \\section{Technology}
 
-MiniLatex is written in \\href{http://elm-lang.org}{Elm}, the statically typed functional
-programming language created by Evan Czaplicki.  Because of its excellent
-\\href{http://package.elm-lang.org/packages/elm-tools/parser/latest}{parser combinator library}, Elm is an ideal choice for a project like the present one.
+MiniLatex is written in \\href{http://elm-lang.org}{Elm},
+the statically typed functional programming language
+created by Evan Czaplicki.  Because of its excellent
+\\href{http://package.elm-lang.org/packages/elm-tools/parser/latest}{parser combinator library},
+Elm is an ideal choice for a project like the present one.
 
 
 For an overview of the design of MiniLatex, see
 \\href{https://hackernoon.com/towards-latex-in-the-browser-2ff4d94a0c08}{Towards Latex in the Browser}.
-Briefly, \\href{https://www.mathjax.org/}{MathJax} is used inside dollar signs, and Elm is used outside.
+Briefly, \\href{https://www.mathjax.org/}{MathJax}
+is used inside dollar signs, and Elm is used outside.
 
 \\bigskip
 
@@ -297,67 +309,115 @@ mathExampleText =
 
 \\section{Bras and Kets}
 
-Paul Dirac invented a new notation \\mdash the notation of bras and kets \\mdash for working with hermitian inner products and operators on a Hilbert space $H$.  We describe the basics below, then elaborate on how these apply in the case of countable and continuous orthonormal bases.  We also discuss some of the mathematical issues raised by the delta function and by formulas such as
+Paul Dirac invented a new notation \\mdash the
+notation of bras and kets \\mdash for working
+with hermitian inner products and operators on
+a Hilbert space $H$.  We describe the basics
+below, then elaborate on how these apply in
+the case of countable and continuous
+orthonormal bases.  We also discuss
+some of the mathematical issues raised
+by the delta function and by formulas such as
 
 \\begin{equation}
 \\int_{-\\infty}^\\infty \\frac{dx}{2\\pi}\\; | x \\ket\\bra x | = 1
 \\end{equation}
-a
-that Dirac introduced and which abound in the physics literature.
+
+that Dirac introduced and which abound in the
+physics literature.
 
 \\subsection{The basics}
 
-For the inner product of two vectors, we write $\\bra u | v \\ket$, where this expression is linear in the second variable and conjugate linear in the first.  Thus Dirac's $\\bra u | v \\ket$ is the mathematicians's $\\bra v, w \\ket$.
+For the inner product of two vectors,
+we write $\\bra u | v \\ket$, where this expression
+is linear in the second variable and conjugate
+linear in the first.  Thus Dirac's
+$\\bra u | v \\ket$ is the mathematicians's $\\bra v, w \\ket$.
 
-In this notations, $\\bra u | c v\\ket = c\\bra u | v \\ket$, but $\\bra cu |  v\\ket = \\bar c\\bra u | v \\ket$. The symmetry condition reads $\\bra v | u \\ket = \\overline{\\bra u  | v \\ket}$.
+In this notations,
+$\\bra u | c v\\ket = c\\bra u | v \\ket$, but $\\bra cu |  v\\ket = \\bar c\\bra u | v \\ket$.
+The symmetry condition reads $\\bra v | u \\ket = \\overline{\\bra u  | v \\ket}$.
 
-Let $A$ be an operator.  The expression $\\bra u | A | v \\ket$ means $\\bra u | Av \\ket$.  Thus it is the same as $\\bra A^*u | v \\ket$.
+Let $A$ be an operator.  The expression
+$\\bra u | A | v \\ket$ means $\\bra u | Av \\ket$.
+Thus it is the same as $\\bra A^*u | v \\ket$.
 
-The symbol $|v\\ket$ has the same meaning as $v$ if $v$ is a vector. Such vectors are called \\term{kets}.  Let $\\set{ \\psi_a }$ be a set of states, that is, eigenvectors for an operator $A$ with eigenvalue $a$.  The notations $\\psi_a$, $|\\psi_a \\ket$, and  $|a\\ket$ all stand fors the same thing.  It makes sense to say
+The symbol $|v\\ket$ has the same meaning as $v$
+if $v$ is a vector. Such vectors are
+called \\term{kets}.  Let $\\set{ \\psi_a }$ be a set
+of states, that is, eigenvectors for an operator
+$A$ with eigenvalue $a$.  The notations
+$\\psi_a$, $|\\psi_a \\ket$, and  $|a\\ket$
+all stand for the same thing.  It makes sense to say
 
 \\begin{equation}
 A|a\\ket = a|a\\ket
 \\end{equation}
 
-The expression $\\bra u |$ is the \\term{bra} associated to $u$, where $u$ is an element of the space \\emph{dual} to $V$.  That is, $u$ is a linear functional on $V$:
+The expression $\\bra u |$ is the \\term{bra}
+associated to $u$, where $u$ is an element of
+the space \\emph{dual} to $V$.  That is,
+$u$ is a linear functional on $V$:
 
 \\begin{equation}
 \\bra u |(v) = u(v)
 \\end{equation}
 
-If $u$ is a vector in $V$, we may define the linear functional $\\phi_u(v) = \\bra u | v \\ket$.  In this case, both sides of the equation
+If $u$ is a vector in $V$, we may define the
+linear functional $\\phi_u(v) = \\bra u | v \\ket$.
+In this case, both sides of the equation
 
 \\begin{equation}
 \\bra u |(v) = \\bra u | v \\ket
 \\end{equation}
 
-have meaning.  For finite-dimensional spaces, we may view bras as row vectors and kets as column vectors.
+have meaning.  For finite-dimensional spaces,
+we may view bras as row vectors and kets
+as column vectors.
 
-Consider next the expression $| v \\ket \\bra v |$ where $v$ is a unit vector. it operates on a arbitrary vector $w$ by the rule
+Consider next the expression $| v \\ket \\bra v |$
+where $v$ is a unit vector. it operates on
+a arbitrary vector $w$ by the rule
 
 \\begin{equation}
   | w \\ket \\mapsto | v \\ket \\bra v | w \\ket
 \\end{equation}
 
-If $| w \\ket$ is orthogonal to $| v \\ket$, then the result is zero.  If $| w \\ket = | v \\ket$, then the result is $| v \\ket$, Therefore $| v \\ket \\bra v |$ is orthogonal projection onto $v$.
+If $| w \\ket$ is orthogonal to $| v \\ket$,
+then the result is zero.
+ If $| w \\ket = | v \\ket$, then the result is $| v \\ket$,
+ Therefore $| v \\ket \\bra v |$ is
+ orthogonal projection onto $v$.
 
 
 \\subsection{Resolution of the identity}
 
-Let $\\set{ |v_n\\ket} = \\set{ |n\\ket}$ be a complete orthonormal set for $H$ which is indexed by integers $n = 0, 1, 2, \\ldots$.  We claim that
+Let $\\set{ |v_n\\ket} = \\set{ |n\\ket}$
+be a complete orthonormal set for $H$
+which is indexed by integers
+$n = 0, 1, 2, \\ldots$.  We claim that
 
 \\begin{equation}
 \\label{resolutionofidentity}
 \\sum_n | n \\ket \\bra n | = \\id.
 \\end{equation}
 
-That is, the expression on the left, which is a sum of projections operators, is the identity operator $\\id$.  We say that the left-hand side of \\eqref{resolutionofidentity} is  \\term{resolution of the identity}.  The proof that  \\eqref{resolutionofidentity} holds an exercise in bra-ket formalism. Let $v$ be arbitrary and write
+That is, the expression on the left, which is
+a sum of projections operators, is the
+identity operator $\\id$.  We say that the
+eft-hand side of \\eqref{resolutionofidentity}
+is  \\term{resolution of the identity}.  The p
+roof that  \\eqref{resolutionofidentity} h
+olds an exercise in bra-ket formalism.
+Let $v$ be arbitrary and write
 
 \\begin{equation}
   v = \\sum_m | m \\ket\\bra m | v \\ket
 \\end{equation}
 
-This is the Fourier decomposition of $v$.  Note that it depends linearly on $| v \\ket$. Applying $\\sum | n \\ket\\bra n|$ to $v$, we find that
+This is the Fourier decomposition of $v$.
+Note that it depends linearly on $| v \\ket$. Applying $\\sum | n \\ket\\bra n|$
+to $v$, we find that
 
 \\begin{align}
 \\left(\\sum_n  | n\\ket \\bra n |\\right) \\left(\\sum_m | m \\ket\\bra m | v \\ket \\right) &=
@@ -369,7 +429,9 @@ This is the Fourier decomposition of $v$.  Note that it depends linearly on $| v
 
 \\strong{Q.E.D.}
 
-Here the quantities $\\delta_{n,m}$ may be viewed as the elements of the identity matrix  \\mdash possibly $n\\times n$, possibly $\\infty\\times\\infty$.
+Here the quantities $\\delta_{n,m}$ may be viewed as the
+elements of the identity matrix
+\\mdash possibly $n\\times n$, possibly $\\infty\\times\\infty$.
 
 Consider next an operator $\\Omega$.  Write it as
 
@@ -385,9 +447,11 @@ The operator  is determined by its \\term{matrix elements}
 \\Omega_{m,n} = \\bra m |  \\Omega | n \\ket
 \\end{equation}
 
-The matrix elements are the elements o the matrix of  $\\Omega$ in the given orthonormal basis, i.e.,  the $\\Omega_{m,n}$
+The matrix elements are the elements o the matrix of
+$\\Omega$ in the given orthonormal basis, i.e.,  the $\\Omega_{m,n}$
 
-One often encounters the phrase "let us insert a resolution of the identity".  To see what this means, consider the expression $\\bra m AB m \\ket$, which we expand as follows:
+One often encounters the phrase
+"let us insert a resolution of the identity" o see what this means, consider the expression $\\bra m AB m \\ket$, which we expand as follows:
 
 \\begin{align}
 \\bra m | AB n | \\ket &= \\bra m | A\\id B | n \\ket \\\\
@@ -402,13 +466,19 @@ This is the same as the identity
 
 \\subsection{Continuous spectrum}
 
-One also has resolution of the identity for operators with continuous spectrum.  Take, for example, the operator $-id/dx$ which has (generalized) eigenfunctions $e^{ikx}$. Writing $| k \\ket$ for $e^{ikx}$, one has
+One also has resolution of the identity for
+operators with continuous spectrum.  Take,
+for example, the operator $-id/dx$ which has
+(generalized) eigenfunctions $e^{ikx}$.
+Writing $| k \\ket$ for $e^{ikx}$, one has
 
 \\begin{equation}
 \\id = \\frac{1}{2\\pi} \\int_{-\\infty}^\\infty dk\\, | k \\ket \\bra k |
 \\end{equation}
 
-Standing by itself, the meaning of this formula is a subtle matter.  However, when applied to a function, we have
+Standing by itself, the meaning of this formula
+is a subtle matter.  However, when applied
+to a function, we have
 
 \\begin{equation}
 | f \\ket = \\frac{1}{2\\pi} \\int_{-\\infty}^\\infty dk\\, | k \\ket \\bra k | f \\ket
